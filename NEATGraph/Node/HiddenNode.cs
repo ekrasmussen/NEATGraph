dc5f54b10ec9de30da5f366fb2dae13ab8addeb4
Emil Rasmussen <emil@erasmussen.dk>
2022-06-12 03:15:21 +0200

    Updated Fire Method to use the IActivationFunction

--- NEATGraph/Node/HiddenNode.cs
+++ NEATGraph/Node/HiddenNode.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NEATGraph.ActivationFunctions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace NEATGraph.Node
 {
     public class HiddenNode : GraphNode
     {
-        public IActivationFunction ActivationFunction { get; set; }
+        public IActivationFunction ActivationFunction { get; set; } = new LinearActivation();
         public HiddenNode(string name) : base(name)
         {
 
@@ -22,8 +23,7 @@ namespace NEATGraph.Node
                 total += edge.GetStrength();
             }
 
-            
-            return total;
+            return ActivationFunction.Calculate(total);
         }
     }
 }
